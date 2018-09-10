8b3657ca11f5de101e4f4fe24f1c7984a8c752c1
Arpan <Arpan@192.168.11.20>
2018-09-10 08:49:07 +0530

    Added logger comments and dependency

--- src/test/java/com/ab/tests/PipeLineTest.java
+++ src/test/java/com/ab/tests/PipeLineTest.java
@@ -1,11 +1,18 @@
 package com.ab.tests;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PipeLineTest {
+
+	private final Logger logger = LoggerFactory.getLogger(PipeLineTest.class);
 
 	@Test
 	public void testMe() {
 		System.out.println("RUN ME");
+		logger.debug("DEBUG");
+		logger.info("INFO");
+		logger.error("ERROR");
 	}
 }
